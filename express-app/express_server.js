e2997c61800fc3315be177fe4d9becb57e0e8323
Christian Beaudrie <bowtree00@gmail.com>
2016-10-15 18:00:06 +0000

    added lots of commenting

--- express-app/express_server.js
+++ express-app/express_server.js
@@ -1,75 +1,55 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 const methodOverride = require('method-override');
-// const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSaltSync(10);
-// use res.cookie to set values on the cookie [in EXPRESS]
-// use cookie-parser to read values from the cookie [in cookie-parser]
- 
+
 // MIDDLEWARE
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-// app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieSession({ name: "session", keys: ["this is a key", "this is another key"]}));
 app.use(bodyParser.urlencoded());
 app.set("view engine", "ejs");
+app.use(methodOverride('_method')); 
+// method-override is needed to be able to do an app.put call
+// in the form action, append '?_method=PUT' to the end of the URL.
+// This code below will look for that ?_method=PUT attribute and execute app.put instead of app.post
+
 
 // DATABASE
 const user_data = {
   "userRandomID": { id: "userRandomID", email: "testemail@gmail.com", password: "$2a$04$kcqFKEbdS0LgBO/sTgCH3.H3zvERfkWOULYAmrV5rRe0hOIEwWQU6" }
 };
 
-var urlDatabase = { 
+const urlDatabase = { 
   "userRandomID": { "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com" }
 };
 
-
 // FUNCTIONS
-var randomLength = 6;
-var acceptableChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const randomLength = 6;
+const acceptableChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-function generateRandomString(length, chars) {
-  var shortName = "";
+function generateRandomString(length, chars) { 
+  let shortName = "";
   
-  for (i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * chars.length);
+  for (let i = 0; i < length; i++) {
+    let randomIndex = Math.floor(Math.random() * chars.length);
     shortName += chars[randomIndex];
   };
+
   return shortName;
+
  };
 
-// method-override is needed to be able to do an app.put call
-// in the form action, append '?_method=PUT' to the end of the URL.
-// This code below will look for that ?_method=PUT attribute and execute app.put instead of app.post
-app.use(methodOverride('_method')); 
-
-
-// ROUTING
-// 
-// BREAD
-// Browse
-// Read
-// Edit
-// Add
-// Delete
-
-
-// CHANGE THIS - make a welcome page - ask what you want to do. See existing URLs or create a new shortURL
+// LISTENERS
 app.get("/", (req, res) => {
   console.log("IN GET /");
-  let user_id = req.session.user_id;
+  let user_id = req.session.user_id; // NOTE: this code gets the user_id and email from the cookie using 'cookie-session'
   let email = req.session.email;
-
-  console.log("in /, user_id: " + user_id);
-  console.log("in /, email: " + email);
-
-  var templateVars = { user_id: user_id, email: email };
+  let templateVars = { user_id: user_id, email: email };
 
   res.render("urls_home", templateVars);
 });
@@ -83,7 +63,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// /urls/new must be above /urls/:id, otherwise the routing 
+// /urls/new must be placed above /urls/:id, otherwise the routing 
 // to /urls/:id/ will think that the 'new' part is the name of an 'id'
 
 app.get("/urls/new", (req,res) => {
@@ -95,12 +75,12 @@ app.get("/urls/new", (req,res) => {
   res.render("urls_new", templateVars);
 });
 
-// ADD
+// -- ADD
 app.post("/urls", (req, res) => {
   console.log("IN POST /urls");
   let user_id = req.session.user_id;
-  var shortURL = generateRandomString(randomLength, acceptableChars);
-  var longURL = req.body.longURL;
+  let shortURL = generateRandomString(randomLength, acceptableChars);
+  let longURL = req.body.longURL;
   
   // DO SOME ERROR CHECKING HERE - if 'string does not start with http://' then add it
   urlDatabase[user_id][shortURL] = longURL;
@@ -108,7 +88,7 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls");
 });
 
-// DELETE
+// -- DELETE
 app.post("/urls/:id/delete", (req, res) => {
   console.log("IN POST /urls/:id/delete");
   let user_id = req.session.user_id;
@@ -118,45 +98,44 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls/");
 });
 
-app.get("/urls/:id", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   console.log("IN GET /urls/:id");
   let user_id = req.session.user_id;
   let email = req.session.email;
-  let shortURL = req.params.id;
+  let shortURL = req.params.shortURL; // grabs the shortURL from the :shortURL parameter in the URL pattern in the GET request
   let templateVars = { user_id: user_id, email: email, shortURL: shortURL, longURL: urlDatabase[user_id][shortURL] };
 
-  if (urlDatabase[user_id][shortURL] == undefined) {
+  if (urlDatabase[user_id][shortURL] === undefined) {
     res.send("Page not found - 404", 404);
   } else {
     res.render("urls_show", templateVars);
  }; 
-
 });
 
-// EDIT
+// -- EDIT
 // the following 'app.put' call is possible because of the method-override package
 // To use the app.put call, be sure to append ?_method=PUT to the end of the action URL
 // in the form field in the appropriate view
-app.put("/urls/:id", (req, res) => {
-  console.log("IN PUT /urls/:id");
+app.put("/urls/:shortURL", (req, res) => {
+  console.log("IN PUT /urls/:shortURL");
   // use 'bodyparser' package to parse the BODY of the POST call
-  // to get the data sent in the post request
+  // to get the data sent in the post request. e.g., let newURL = req.body.newUrl;
   
   let user_id = req.session.user_id;
-  let newURL = req.body.newUrl;
-  let shortURL = req.params.id;
+  let newURL = req.body.newUrl;       // grabs the newURL from the request body (using body-parser)
+  let shortURL = req.params.shortURL; // grabs the shortURL from the :shortURL parameter in the URL pattern in the GET request
 
   urlDatabase[user_id][shortURL] = newURL;
 
   res.redirect("/urls/");
 });
 
-// redirect to long URL
+// Redirect to long URL
 app.get("/u/:shortURL", (req, res) => {
   console.log("IN GET /u/:shortURL");
-  let shortURL = req.params.shortURL;
+  let shortURL = req.params.shortURL; // grabs the shortURL from the :ID parameter in the URL pattern in the GET request
 
-  for (item in urlDatabase) {
+  for (let item in urlDatabase) {
     if (urlDatabase[item][shortURL]) { // if short url exists, redirect to longURL
       let longURL = urlDatabase[item][shortURL];
 
@@ -169,7 +148,6 @@ app.get("/u/:shortURL", (req, res) => {
     }
   }
   
-  // if nothing is found, send 404 response
   res.send("Page not found - 404", 404);
 });
 
@@ -187,26 +165,26 @@ app.post("/login", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
 
-  for (var item in user_data) { 
+  for (let item in user_data) { 
 
-    if (user_data[item].email === email) {
+    if (user_data[item].email === email) { // check if email exists in the database
       console.log("Email exists in database");
       
-      if (bcrypt.compareSync(password, user_data[item].password)) {
-        // res.cookie("user_id", user_data[item].id); // sets cookie to user_id;
-        // res.cookie("email", email); // sets cookie to user_id;
-        
+      if (bcrypt.compareSync(password, user_data[item].password)) { // check if password matches the database
+   
         // SET cookies
         req.session.user_id = user_data[item].id;
         req.session.email = email;
 
         res.redirect("/urls");
+        
         return;
 
       } else {
 
         templateVars = { message: "The password is incorrect" };
         res.status(403).render("urls_login", templateVars);
+        
         return;
       }
     }
@@ -214,35 +192,33 @@ app.post("/login", (req, res) => {
 
       templateVars = { message: "That user account does not exist" };
       res.status(403).render("urls_login", templateVars);
+      
       return;
-}) 
+}); 
 
 app.post("/logout", (req, res) => {
   console.log("IN POST /logout");
 
-  // res.cookie("user_id", "");
-  // res.cookie("email", "");
-
+    // CLEAR cookies
     req.session.user_id = "";
     req.session.email = "";
 
-  // console.log("user_data: " + JSON.stringify(user_data));
   res.redirect("/");
-})
+});
 
 app.get("/register", (req, res) => {
   console.log("IN GET /register");
 
   let templateVars = { message: undefined };
   res.render("urls_register", templateVars);
-})
+});
 
 app.post("/register", (req, res) => {
   console.log("IN POST /register");
   const email = req.body.email;
   const password = req.body.password;
 
-  const hashed_password = bcrypt.hashSync(password, salt);
+  const hashed_password = bcrypt.hashSync(password, salt); // create hashed password
 
   let templateVars = { message: undefined, email: email };
 
@@ -251,12 +227,12 @@ app.post("/register", (req, res) => {
   console.log("email: " + email);
   console.log("hashed_password: " + hashed_password);
 
-  for (var item in user_data) { 
-    if (user_data[item].email === email) {
+  for (let item in user_data) { 
+    if (user_data[item].email === email) { // check if email already exists
 
       templateVars["message"] = "An account already exists for that email address. Please use a different one.";
       res.status(400).render("urls_register", templateVars);
-      return; // need to do this since using middleware
+      return; 
     }
   }
 
@@ -268,14 +244,12 @@ app.post("/register", (req, res) => {
 
   let user_id = generateRandomString(10, acceptableChars); // Randomly generate a user id
   
-  // res.cookie("user_id", user_id); // create user_id cookie 
-  // res.cookie("email", email); // puts email in the cookie
-  
+  // SET cookies for new registrant - signs them in after registration
   req.session.user_id = user_id;
   req.session.email = email;
 
-  user_data[user_id] = { id: user_id, email: email, password: hashed_password };
-  urlDatabase[user_id] = {};
+  user_data[user_id] = { id: user_id, email: email, password: hashed_password }; // add them to the user database
+  urlDatabase[user_id] = {}; // give them an empty object to start with
   res.redirect("/");
 });
 
